4a8c782795d6481f1a9e019836590eb6899adb74
farabek <farhodmuhamadiev4@gmail.com>
2024-07-29 13:56:32 +0500

    modified Header.tsx

--- src/layout/header/headerMenu/HeaderMenu.tsx
+++ src/layout/header/headerMenu/HeaderMenu.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { theme } from '../../../styles/Theme';
 
 export const HeaderMenu = (props: { menuItems: Array<string> }) => {
   return (
@@ -7,11 +8,17 @@ export const HeaderMenu = (props: { menuItems: Array<string> }) => {
       <ul>
         {props.menuItems.map((item, index) => {
           return (
-            // <li key={index}>
-            //   <a href="">{item}</a>
-            // </li>
             <ListItem key={index}>
-              <Link href="">{item}</Link>
+              <Link href="">
+                {item}
+
+                <Mask>
+                  <span>{item}</span>
+                </Mask>
+                <Mask>
+                  <span>{item}</span>
+                </Mask>
+              </Link>
             </ListItem>
           );
         })}
@@ -28,12 +35,61 @@ const StyledHeaderMenu = styled.nav`
   }
 `;
 
-const ListItem = styled.li``;
-
 const Link = styled.a`
   font-family: 'Josefin Sans', sans-serif;
   font-weight: 400;
   font-size: 30px;
   text-align: center;
-  color: #757205;
+  color: transparent;
+`;
+
+const Mask = styled.span`
+  position: absolute;
+  top: o;
+  left: 0;
+  display: inlibe-block;
+  height: 50%;
+  overflow-y: hidden;
+//   outline: 1px solid red;
+  color: ${theme.colors.accent};
+
+  & + & {
+    top: 50%;
+    span {
+      display: inline-block;
+      transform: translateY(-50%);
+    }
+`;
+
+const ListItem = styled.li`
+  position: relative;
+
+  &::before {
+    content: '';
+    display: inline-block;
+    height: 3px;
+    background-color: ${theme.colors.accent};
+
+    position: absolute;
+    top: 50%;
+    left: -10px;
+    right: -10px;
+    z-index: 1;
+
+    transform: scale(0);
+  }
+
+  &:hover {
+    &::before {
+      transform: scale(1);
+    }
+
+    ${Mask} {
+      transform: skewX(12deg) translateX(5px);
+
+      & + ${Mask} {
+        transform: skewX(12deg) translateX(-5px);
+      }
+    }
+  }
 `;
