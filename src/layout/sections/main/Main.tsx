31ee921e5d5fd5d9c8142dc4f2330e55e3e2c493
farabek <farhodmuhamadiev4@gmail.com>
2024-08-09 21:51:57 +0500

    modified Main.tsx

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,127 +1,9 @@
-/* import photo from '../../../assets/images/photo.jpg';
-import styled from 'styled-components';
-import { FlexWrapper } from '../../../components/FlexWrapper';
-import { Container } from '../../../components/Container';
-import { theme } from '../../../styles/Theme';
-import { font } from '../../../styles/Common';
-
-export const Main: React.FC = () => {
-  return (
-    <StyledMain>
-      <Container>
-        <FlexWrapper align={'center'} justify={'space-around'} wrap={'wrap'}>
-          <div>
-            <SmallText>Hi There</SmallText>
-            <Name>
-              I am <span>Svetlana Dyablo</span>
-            </Name>
-            <MainTitle>A Web Developer. </MainTitle>
-          </div>
-
-          <PhotoWrapper>
-            <Photo src={photo} alt="" />
-          </PhotoWrapper>
-        </FlexWrapper>
-      </Container>
-    </StyledMain>
-  );
-};
-
-const StyledMain = styled.section`
-  min-height: 100vh;
-  background-color: #fff5e7;
-  display: flex;
-`;
-
-const PhotoWrapper = styled.div`
-  position: relative;
-  z-index: 0;
-
-  &::before {
-    content: '';
-    width: 360px;
-    height: 470px;
-    border: 5px solid ${theme.colors.accent};
-
-    position: absolute;
-    top: -24px;
-    left: 24px;
-    z-index: -1;
-
-    @media ${theme.media.mobile} {
-      width: 314px;
-      height: 414px;
-      top: -17px;
-      left: 20px;
-    }
-  }
-
-  @media ${theme.media.mobile} {
-    margin-top: 65px;
-  }
-`;
-
-const Photo = styled.img`
-  width: 350px;
-  height: 430px;
-  object-fit: cover;
-  margin-right: 20px;
-
-  @media ${theme.media.mobile} {
-    width: 310px;
-    height: 380px;
-  }
-`;
-
-const MainTitle = styled.h1`
-  // font-weight: 400;
-  // font-size: 27px;
-  ${font({ weight: 400, Fmax: 27, Fmin: 20 })};
-`;
-
-const Name = styled.h2`
-  ${font({ family: "'Josefin Sans', sans-serif", weight: 700, Fmax: 50, Fmin: 36 })}
-  // font-family: 'Josefin Sans', sans-serif;
-  // font-weight: 700;
-  // font-size: 50px;
-  letter-spacing: 0.05em;
-  margin: 10px 0;
-
-  span {
-    position: relative;
-    z-index: 0;
-    white-space: nowrap;
-
-    &::before {
-      content: '';
-      display: inline-block;
-      width: 100%;
-      height: 20px;
-      background-color: ${theme.colors.accent};
-
-      position: absolute;
-      bottom: 0;
-      z-index: -1;
-    }
-  }
-
-  @media ${theme.media.mobile} {
-    margin: 15px 0 22px;
-  }
-`;
-
-const SmallText = styled.h2`
-  font-weight: 400;
-  font-size: 14px;
-`;
- */
-
-//////////////////////////////////////////////////
-
 import photo from '../../../assets/images/photo.jpg';
 import { FlexWrapper } from '../../../components/FlexWrapper';
 import { Container } from '../../../components/Container';
 import { S } from './Main_Styles';
+import Typewriter from 'typewriter-effect';
+import Tilt from 'react-parallax-tilt';
 
 export const Main: React.FC = () => {
   return (
@@ -133,12 +15,32 @@ export const Main: React.FC = () => {
             <S.Name>
               I am <span>Svetlana Dyablo</span>
             </S.Name>
-            <S.MainTitle>A Web Developer. </S.MainTitle>
+
+            <S.MainTitle>
+              <p>A Web Developer.</p>
+              <Typewriter
+                options={{
+                  strings: ['A Web Developer.', 'A Frontend Developer', 'A Web Designer'],
+                  autoStart: true,
+                  loop: true,
+                  delay: 150,
+                }}
+              />
+            </S.MainTitle>
           </div>
 
-          <S.PhotoWrapper>
-            <S.Photo src={photo} alt="" />
-          </S.PhotoWrapper>
+          <Tilt
+            className="parallax-effect-img"
+            tiltMaxAngleX={40}
+            tiltMaxAngleY={40}
+            perspective={800}
+            transitionSpeed={1500}
+            scale={1.1}
+            gyroscope={true}>
+            <S.PhotoWrapper>
+              <S.Photo src={photo} alt="" />
+            </S.PhotoWrapper>
+          </Tilt>
         </FlexWrapper>
       </Container>
     </S.Main>
